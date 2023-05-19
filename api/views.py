0ed53d391e853a21b650caa5f9c7f26acab4fa18
seva <lopachuk.seva@gmail.com>
2023-05-19 21:36:42 +0200

    new queryset in NoteAPIList

--- api/views.py
+++ api/views.py
@@ -3,11 +3,16 @@ from rest_framework.permissions import IsAuthenticated
 
 from api.models import Note
 from api.serializers import NoteSerializer
+from rest_framework.authtoken.models import Token
 
 
 class NoteAPIList(generics.ListCreateAPIView):
+    def get_queryset(self):
+        return Note.objects.filter(user=self.request.user.id)
 
-    # self.request.query_params.get('your_key_name', None)
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
     queryset = Note.objects.all()
     serializer_class = NoteSerializer
     # permission_classes = (IsAuthenticated, )
